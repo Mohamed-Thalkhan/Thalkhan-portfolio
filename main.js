5c6eb2a8b0588a5686ab6bea552440541f95ff0f
Mohamed Thalkhan <147623609+Mohamed-Thalkhan@users.noreply.github.com>
2023-10-16 12:48:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "College Student", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backdDelay:1000,
+    loop:true
+});
